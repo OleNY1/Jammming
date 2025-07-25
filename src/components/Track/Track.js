49092018a10f441894a9fa5d28a5cc1946f42b41
Oleh Goumain <olehgoumain@Olehs-MBP.lan>
2025-07-24 22:50:22 -0400

    Reinitialize Git and restore project tracking

---
+++ src/components/Track/Track.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Track() {
+  return (
+    <div>
+      <p>Track Name | Artist | Album</p>
+      <button>+</button>
+    </div>
+  );
+}
+
+export default Track;
